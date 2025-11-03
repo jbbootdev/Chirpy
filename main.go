909d55078af5953259d5c88fe623008249feb02d
James <madhatter84gn@fastmail.com>
2025-11-03 11:14:11 -0500

    completed chirpy intial work

--- main.go
+++ main.go
@@ -1,21 +1,101 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) adminMetricsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	html := fmt.Sprintf(`
+		<html>
+		<body>
+		<h1>Welcome, Chirpy Admin</h1>
+		<p>Chirpy has been visited %d times!</p>
+		</body>
+		</html>`, cfg.fileserverHits.Load())
+	w.Write([]byte(html))
+}
+
+// func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
+// 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+// 	fmt.Fprintf(w, "Hits: %d", cfg.fileserverHits.Load())
+// }
+
+func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0) // Reset the counter
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	fmt.Fprintln(w, "Hits reset to 0")
+}
+
+type chirpRequest struct {
+	Body string `json:"body"`
+}
+
+func (cfg *apiConfig) validateChirpHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		jsonResponse(w, http.StatusMethodNotAllowed, "Something went wrong")
+		return
+	}
+
+	var request chirpRequest
+	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
+		jsonResponse(w, http.StatusBadRequest, "Something went wrong")
+		return
+	}
+
+	// Validate chirp length
+	if len(request.Body) > 140 {
+		jsonResponse(w, http.StatusBadRequest, "Chirp is too long")
+		return
+	}
+
+	// If valid
+	jsonResponse(w, http.StatusOK, map[string]bool{"valid": true})
+}
+
+func jsonResponse(w http.ResponseWriter, statusCode int, response interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+
+	jsonResponseBody, err := json.Marshal(response)
+	if err != nil {
+		http.Error(w, "Something went wrong", http.StatusInternalServerError)
+		return
+	}
+
+	w.Write(jsonResponseBody)
+}
 
 func main() {
 	mux := http.NewServeMux()
+	apiCfg := &apiConfig{}
 
-	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("GET /api/healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("OK"))
 	})
 
-	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
-
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(".")))))
 	mux.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets/"))))
+	mux.HandleFunc("GET /admin/metrics", apiCfg.adminMetricsHandler)
+	mux.HandleFunc("POST /admin/reset", apiCfg.resetHandler)
+	mux.HandleFunc("/api/validate_chirp", apiCfg.validateChirpHandler)
 
 	server := &http.Server{
 		Addr:    ":8080",
